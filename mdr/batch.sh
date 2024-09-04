6876a2cc4a9f8585296fc64b142cc8340e683577
Martin Craig <martin.craig@nottingham.ac.uk>
2024-09-04 11:48:40 +0100

    MDR scripts

---
+++ mdr/batch.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#SBATCH --time=04:00:00
+#SBATCH --job-name=mdr_compare
+#NOTSBATCH --partition=imgcomputeq,imghmemq
+#SBATCH --partition=imghmemq,imgcomputeq,imgvoltaq,imgpascalq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=16g
+#SBATCH --qos=img
+#NOTSBATCH --gres=gpu:1
+#SBATCH --export=NONE
+#SBATCH --array=0-500
+#SBATCH --output mdr/logs/%A_%a.out
+#SBATCH --error mdr/logs/%A_%a.err
+
+module load renal-preproc-img
+module load fsl-img/6.0.6.3
+module load conda-img
+source activate renal_preproc
+
+MODELDIR=/spmstore/project/RenalMRI/trained_models/
+OUTDIR=/spmstore/project/RenalMRI/mdr/proc_20240522
+#SLURM_ARRAY_TASK_ID=18
+
+echo "Processing subject ${SLURM_ARRAY_TASK_ID}"
+
+python pipelines/mdr_compare.py --input ${OUTDIR} --input-subfolder fsort --output ${OUTDIR} --output-subfolder compare --overwrite \
+                                 --subjidx ${SLURM_ARRAY_TASK_ID} 
+
---
+++ mdr/postrun.sh
@@ -0,0 +1,3 @@
+fproc-combine --input  /spmstore/project/RenalMRI/mdr/proc_20240522/ --output /spmstore/project/RenalMRI/mdr/stats_20240522.csv --path compare/stats/stats.csv compare/def/stats.csv
+#fproc-flatten --input  /spmstore/project/RenalMRI/bariatric/proc/ --output /spmstore/project/RenalMRI/bariatric/imgs
+
