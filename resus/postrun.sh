cd6b44a8ef378ea1c7462c5234c4a49ab252d80a
Martin Craig <martin.craig@nottingham.ac.uk>
2024-09-04 11:47:51 +0100

    RESUS scripts

---
+++ resus/postrun.sh
@@ -0,0 +1,3 @@
+fproc-flatten --input /spmstore/project/RenalMRI/resus/proc/ --output /spmstore/project/RenalMRI/resus/imgs/
+fproc-combine --input /spmstore/project/RenalMRI/resus/proc/ --output /spmstore/project/RenalMRI/resus/stats.csv --path stats/stats.csv
+
