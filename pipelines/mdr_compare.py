127519221e4cffe41f0324f03403af8cdc3d55de
Martin Craig <martin.craig@nottingham.ac.uk>
2024-09-04 11:33:52 +0100

    Implement deformation measures

--- pipelines/mdr_compare.py
+++ pipelines/mdr_compare.py
@@ -1,3 +1,4 @@
+import collections
 import logging
 
 import numpy as np
@@ -7,6 +8,7 @@ from fsort.image_file import ImageFile
 from fproc.options import ArgumentParser
 from fproc.pipeline import Pipeline
 from fproc.module import Module, StatsModule
+import fproc.stats as stats
 
 __version__ = "0.0.1"
 
@@ -46,6 +48,8 @@ class T1Clean(Module):
 
     def _clean_t2w(self, t1_seg, t2w_mask):
         mask_data = self.resample(t2w_mask, t1_seg, is_roi=True, allow_rotated=True).get_fdata()
+        if np.count_nonzero(mask_data) == 0:
+            LOG.warn(f" - T2w mask zero voxel count when regridded onto T1. T2w and T1 scans probably do not align")
 
         # Dilate T2w masks by 2 voxels
         mask_data = scipy.ndimage.binary_dilation(mask_data, structure=np.ones((3, 3, 3)))
@@ -76,12 +80,89 @@ class T1Clean(Module):
             else:
                 self.lightbox(t1_map_mdr.data, cleaned_data_t1_seg, cleaned_basename + "_lightbox")
 
-class Overlays(Module):
+class Deformation(Module):
     def __init__(self):
-        Module.__init__(self, "overlays")
+        Module.__init__(self, "def")
+
+    STATS = {
+        "n" : stats.n, "iqn" : stats.iqn, "mean" : stats.mean, "iqmean" : stats.iqmean, 
+        "std" : stats.std, "iqstd" : stats.iqstd
+    }
 
     def process(self):
-        pass
+        # For max/min Jacobians subtract 1 (so range becomes -1 to +1)
+        for fname in ("jac_max", "jac_min", "jac"):
+            img = self.inimg("def", f"{fname}.nii.gz")
+            data_sub_1 = img.data - 1
+            img.save_derived(data_sub_1, self.outfile(f"{fname}_sub.nii.gz"))
+        
+        def_max = self.inimg("def", "def_max.nii.gz")
+        def_norm = self.inimg("def", "def_norm.nii.gz")
+        jac_max = self.inimg("def", "jac_max_sub.nii.gz", is_depfile=True)
+        jac_min = self.inimg("def", "jac_min_sub.nii.gz", is_depfile=True)
+        jac = self.inimg("def", "jac_sub.nii.gz", is_depfile=True)
+
+        LOG.info(f" - Resampling TKV segmentations onto deformation grid from {def_max.fname}")
+        masks = {"all" : np.ones(def_max.shape, dtype=int)}
+        for mask_name in ("left", "right"):
+            masks[mask_name] = self.resample(self.inimg("tkv", f"tkv_{mask_name}.nii.gz"), def_max, is_roi=True, allow_rotated=True).get_fdata()
+            LOG.info(f" - {mask_name} has {np.count_nonzero(masks[mask_name])} voxels")
+
+        stats_data = collections.OrderedDict()
+        n_slices = def_max.shape[2]
+        n_tis = def_norm.shape[3]
+        for mask_name, mask in masks.items():
+            LOG.info(f" - Processing mask: {mask_name}")
+            max_def_slice, max_def_slice_idx = 0, -1
+            for slice_idx in range(n_slices):
+                # For each slice, compute in the TKV kidney mask L and R separately, and for the whole image: 
+                #   IQmean and stdev of the Max deformation image.
+                #   Iqmean and stdev: 
+                #     Max Jacobian image (positive voxels and negative voxels and the number of each)
+                #     Min Jacobian image (positive voxels and negative voxels and the number of each)
+                LOG.info(f"   - slice: {slice_idx}")
+                def_max_slicedata = def_max.data[..., slice_idx][mask[..., slice_idx] > 0]
+                jac_max_slicedata = jac_max.data[..., slice_idx][mask[..., slice_idx] > 0]
+                jac_min_slicedata = jac_min.data[..., slice_idx][mask[..., slice_idx] > 0]
+                jac_max_slicedata_pos = jac_max_slicedata[jac_max_slicedata >= 0]
+                jac_max_slicedata_neg = jac_max_slicedata[jac_max_slicedata < 0]
+                jac_min_slicedata_pos = jac_min_slicedata[jac_min_slicedata >= 0]
+                jac_min_slicedata_neg = jac_min_slicedata[jac_min_slicedata < 0]
+                max_def = stats.mean(def_max_slicedata)
+                if max_def_slice_idx == -1 or max_def_slice < max_def:
+                    max_def_slice, max_def_slice_idx = max_def, slice_idx
+                for data_name, arr in {
+                    "def_max" : def_max_slicedata, "jac_max_pos" : jac_max_slicedata_pos,
+                    "jac_max_neg" : jac_max_slicedata_neg, "jac_min_pos" : jac_min_slicedata_pos,
+                    "jac_min_neg" : jac_min_slicedata_neg,
+                }.items():
+                    for stat_name, impl in self.STATS.items():
+                        stats_data[f"{data_name}_s{slice_idx}_{mask_name}_{stat_name}"] = impl(arr)
+
+            # For the slice with greatest Max deformation: 
+            # Then use the deformation norm file and Jacobian file, estimate for each TI:
+            LOG.info(f" - Max deformation found in slice: {max_def_slice_idx}: {max_def_slice}")
+            stats_data[f"def_max_slice_idx_{mask_name}"] = max_def_slice_idx
+            for ti_idx in range(n_tis):
+                # Max deformation, Max Jacobian and Min Jacobian.
+                # Max Jacobian image (positive voxels and negative voxels and the number of each)
+                # Min Jacobian image (positive voxels and negative voxels and the number of each)
+                LOG.info(f"   - TI: {ti_idx}")
+                def_norm_tidata = def_norm.data[..., max_def_slice_idx, ti_idx][mask[..., max_def_slice_idx] > 0]
+                jac_tidata = jac.data[..., max_def_slice_idx, ti_idx][mask[..., max_def_slice_idx] > 0]
+                jac_tidata_pos = jac_tidata[jac_tidata >= 0]
+                jac_tidata_neg = jac_tidata[jac_tidata < 0]
+                for data_name, arr in {
+                    "def_norm" : def_norm_tidata, "jac_pos" : jac_tidata_pos, "jac_neg" : jac_tidata_neg
+                }.items():
+                    for stat_name, impl in self.STATS.items():
+                        stats_data[f"{data_name}_t{ti_idx}_{mask_name}_{stat_name}"] = impl(arr)
+
+        stats_path = self.outfile("stats.csv")
+        LOG.info(f" - Saving stats to {stats_path}")
+        with open(stats_path, "w") as stats_file:
+            for name, value in stats_data.items():
+                stats_file.write(f"{name},{str(value)}\n")
 
 class Stats(StatsModule):
     def __init__(self):
@@ -171,14 +252,14 @@ class Stats(StatsModule):
 MODULES = [
     T1Seg(),
     T1Clean(),
-    #Overlays(),
+    Deformation(),
     Stats(),
 ]
 
 class MdrCompareArgumentParser(ArgumentParser):
     def __init__(self):
         ArgumentParser.__init__(self, "mdr_compare", __version__)
-        self.add_argument("--t1-model", help="Filename or URL for T1 segmentation model weights", default="/software/imaging/ukbbseg/ukbb-mri-sseg/trained_models/kidney_t1.pt")
+        self.add_argument("--t1-model", help="Filename or URL for T1 segmentation model weights", default="/software/imaging/ukbbseg/ukbb-mri-sseg/trained_models/kidney_t1_min_max.pt")
 
 class MdrCompare(Pipeline):
     def __init__(self):
