18eb05c6d272a2fa08a439073a8a6136f6a47fd7
Martin Craig <martin.craig@nottingham.ac.uk>
2024-11-21 15:34:33 +0000

    No longer hardcoding TIs

--- pipelines/mollihr.py
+++ pipelines/mollihr.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import logging
 
 import numpy as np
@@ -22,6 +23,29 @@ class T1(Module):
             conf_fname = t1_map.fname.replace("map", "conf")
             t1_conf = self.inimg("molli",conf_fname , src=self.INPUT)
             t1_conf.save(self.outfile(conf_fname))
+
+class MolliRaw5(maps.T1Molli):
+    def __init__(self):
+        Module.__init__(self, "molli_raw_5")
+
+    def process(self):
+        molli_raw = self.inimgs("molli_raw", "molli_raw*.nii.gz", src=self.INPUT)
+        if not molli_raw:
+            self.no_data("No raw MOLLI data found")
+
+        for molli in molli_raw:
+            if molli.nvols != 7:
+                LOG.warn(f"{molli.fname} has {molli.nvols} volumes - expected 7. Ignoring")
+                continue
+            if not molli.inversiontimedelay or len(molli.inversiontimedelay) < 7:
+                LOG.warn(f"{molli.fname} did not contain at least 7 inversion times: {molli.inversiontimedelay} - ignoring")
+                continue
+
+            data = np.stack([molli.data[..., v] for v in [0, 3, 4, 5, 6]], axis=-1)
+            tis = [molli.inversiontimedelay[t] for t in [0, 3, 4, 5, 6]]
+            molli.metadata["InversionTimeDelay"] = tis
+            LOG.info(f" - {molli.fname} selecting TIs: {tis}")
+            molli.save_derived(data, self.outfile(molli.fname.replace(".nii.gz", "_5.nii.gz")))
 
 class SegStats(statistics.SegStats):
     def __init__(self):
@@ -56,29 +80,28 @@ class SegStats(statistics.SegStats):
             },
         }
 
-        params = {
-            "ff" : {
-                "dir" : "fat_fraction",
-                "glob" : "fat_fraction_orig.nii.gz",
-                "limits" : (0, 1),
-                "segs" : list(segs.keys())
-            },
-            "t2star" : {
-                "dir" : "t2star",
-                "glob" : "t2star_exclude_fill.nii.gz",
-                "limits" : (2, 100),
-                "segs" : list(segs.keys())
-            },
-            "b0" : {
-                "dir" : "b0",
-                "glob" : "b0.nii.gz",
-                "segs" : list(segs.keys())
-            },
-            "b1" : {
-                "dir" : "b1",
-                "glob" : "b1.nii.gz",
-                "segs" : list(segs.keys())
-            },
+        params = OrderedDict()
+        params["ff"] = {
+            "dir" : "fat_fraction",
+            "glob" : "fat_fraction_orig.nii.gz",
+            "limits" : (0, 1),
+            "segs" : list(segs.keys())
+        }
+        params["t2star"] = {
+            "dir" : "t2star",
+            "glob" : "t2star_exclude_fill.nii.gz",
+            "limits" : (2, 100),
+            "segs" : list(segs.keys())
+        }
+        params["b0"] = {
+            "dir" : "b0",
+            "glob" : "b0.nii.gz",
+            "segs" : list(segs.keys())
+        }
+        params["b1"] = {
+            "dir" : "b1",
+            "glob" : "b1.nii.gz",
+            "segs" : list(segs.keys())
         }
 
         for idx in range(1, 21):
@@ -101,38 +124,46 @@ class SegStats(statistics.SegStats):
                 },
             })
 
-            params.update({
-                f"t1_{idx}" : {
-                    "dir" : "t1",
-                    "glob" : f"t1_map_{idx}.nii.gz",
-                    "segs" : [
-                        f"kidney_cortex_l_{idx}",
-                        f"kidney_cortex_r_{idx}",
-                        f"kidney_medulla_l_{idx}",
-                        f"kidney_medulla_r_{idx}",
-                    ]
-                },
-                f"t1_molli_5tis_{idx}" : {
-                    "dir" : "t1_molli_5",
-                    "glob" : f"molli_raw_{idx}_t1_map.nii.gz",
-                    "segs" : [
-                        f"kidney_cortex_l_{idx}",
-                        f"kidney_cortex_r_{idx}",
-                        f"kidney_medulla_l_{idx}",
-                        f"kidney_medulla_r_{idx}",
-                    ]
-                },
-                f"t1_molli_7tis_{idx}" : {
-                    "dir" : "t1_molli_7",
-                    "glob" : f"molli_raw_{idx}_t1_map.nii.gz",
-                    "segs" : [
-                        f"kidney_cortex_l_{idx}",
-                        f"kidney_cortex_r_{idx}",
-                        f"kidney_medulla_l_{idx}",
-                        f"kidney_medulla_r_{idx}",
-                    ]
-                }
-            })
+            params[f"t1_{idx}"] = {
+                "dir" : "t1",
+                "glob" : f"t1_map_{idx}.nii.gz",
+                "segs" : [
+                    f"kidney_cortex_l_{idx}",
+                    f"kidney_cortex_r_{idx}",
+                    f"kidney_medulla_l_{idx}",
+                    f"kidney_medulla_r_{idx}",
+                    "spleen",
+                    "liver",
+                ]
+            }
+
+        for idx in range(1, 21):
+            params[f"t1_molli_5tis_{idx}"] = {
+                "dir" : "t1_molli_5",
+                "glob" : f"molli_raw_{idx}_5_t1_map.nii.gz",
+                "segs" : [
+                    f"kidney_cortex_l_{idx}",
+                    f"kidney_cortex_r_{idx}",
+                    f"kidney_medulla_l_{idx}",
+                    f"kidney_medulla_r_{idx}",
+                    "spleen",
+                    "liver",
+                ]
+            }
+
+        for idx in range(1, 21):
+            params[f"t1_molli_7tis_{idx}"] = {
+                "dir" : "t1_molli_7",
+                "glob" : f"molli_raw_{idx}_t1_map.nii.gz",
+                "segs" : [
+                    f"kidney_cortex_l_{idx}",
+                    f"kidney_cortex_r_{idx}",
+                    f"kidney_medulla_l_{idx}",
+                    f"kidney_medulla_r_{idx}",
+                    "spleen",
+                    "liver",
+                ]
+            }
 
         statistics.SegStats.__init__(
             self, name="stats", 
@@ -158,8 +189,9 @@ MODULES = [
     CopyModule("b1"),
     maps.T2starDixon(),
     T1(),
-    maps.T1Molli(name="t1_molli_5", tis=[117, 1017, 2017, 3017, 4017]),
-    maps.T1Molli(name="t1_molli_7", tis=[117, 1017, 2017, 3017, 4017, 159, 201]),
+    MolliRaw5(),
+    maps.T1Molli(name="t1_molli_5", molli_dir="molli_raw_5", molli_src=Module.OUTPUT),
+    maps.T1Molli(name="t1_molli_7"),
     # Segmentation cleaning
     seg_postprocess.KidneyT1Clean(t2w=False),
     # Statistics
