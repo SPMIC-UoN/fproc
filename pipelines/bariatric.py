9ab6b252ef412a26100bc427c70d254ebd44e7ab
Martin Craig <martin.craig@nottingham.ac.uk>
2026-06-15 10:23:01 +0100

    Add T2 and take pancreas from totalseg

--- pipelines/bariatric.py
+++ pipelines/bariatric.py
@@ -15,10 +15,10 @@ LOG = logging.getLogger(__name__)
 
 class PancreasSegEro(Module):
     def __init__(self):
-        Module.__init__(self, "seg_pancreas_ethrive_ero")
+        Module.__init__(self, "seg_pancreas_ero")
 
     def process(self):
-        seg_origs = self.inimgs("seg_pancreas_ethrive_fix", "pancreas.nii.gz", is_depfile=True)
+        seg_origs = self.inimgs("totalseg_fix", "pancreas.nii.gz", is_depfile=True)
         if not seg_origs:
             self.no_data("No pancreas segementations found")
         else:
@@ -37,11 +37,11 @@ class PancreasSegEro(Module):
 
 class PancreasSegRestricted(Module):
     def __init__(self):
-        Module.__init__(self, "seg_pancreas_ethrive_restrict")
+        Module.__init__(self, "seg_pancreas_restrict")
 
     def process(self):
-        seg_orig = self.inimg("seg_pancreas_ethrive_fix", "pancreas.nii.gz", is_depfile=True)
-        ff = self.inimg("fat_fraction", "fat_fraction.nii.gz", is_depfile=True)
+        seg_orig = self.inimg("totalseg_fix", "pancreas.nii.gz", is_depfile=True)
+        ff = self.inimg("fat_fraction", "fat_fraction_scanner.nii.gz", is_depfile=True)
         ff_resamp = self.resample(ff, seg_orig, is_roi=False).get_fdata().squeeze()
         ff_30 = ff_resamp < 30
         ff_50 = ff_resamp < 50
@@ -53,9 +53,9 @@ class PancreasSegRestricted(Module):
         seg_orig.save_derived(seg_30, self.outfile("pancreas_ff_lt_30.nii.gz"))
         seg_orig.save_derived(seg_50, self.outfile("pancreas_ff_lt_50.nii.gz"))
 
-class T2(CopyModule):
+class T2Scanner(CopyModule):
     def __init__(self):
-        CopyModule.__init__(self, "t2")
+        CopyModule.__init__(self, "t2_scanner", in_dir="t2", in_name="t2")
 
 class T1Liver(Module):
     def __init__(self):
@@ -94,6 +94,7 @@ class SeT1Map(Module):
                 if len(maps) > 1:
                     LOG.warn(f" - Multiple SE T1 maps found for {self.pipeline.options.subjid}: {maps} - using first")
                 map = ImageFile(maps[0])
+                LOG.info(" - Found SE T1 map: %s" % maps[0])
                 map.save(self.outfile("se_t1.nii.gz"))
 
 class AdcMap(Module):
@@ -115,6 +116,7 @@ class AdcMap(Module):
                 if len(maps) > 1:
                     LOG.warn(f" - Multiple ADC maps found for {self.pipeline.options.subjid}: {maps} - using first")
                 map = ImageFile(maps[0])
+                LOG.info(" - Found ADC map: %s" % maps[0])
                 map.save(self.outfile("adc.nii.gz"))
 
 class SegStats(statistics.SegStats):
@@ -127,31 +129,30 @@ class SegStats(statistics.SegStats):
                     "glob" : "liver.nii.gz"
                 },
                 "spleen" : {
-                    "dir" : "seg_spleen_dixon",
+                    "dir" : "seg_spleen_dixon_fix",
                     "glob" : "spleen.nii.gz"
                 },
                 "pancreas_nonero" : {
-                    "dir" : "seg_pancreas_ethrive_fix",
+                    "dir" : "totalseg_fix",
                     "glob" : "pancreas.nii.gz",
-                    "params" : []  # Volumes only
                 },
                 "pancreas" : {
                     # Eroded pancreas seg for params
-                    "dir" : "seg_pancreas_ethrive_ero",
+                    "dir" : "seg_pancreas_ero",
                     "glob" : "pancreas.nii.gz",
                 },
                 "pancreas_ff_lt_30" : {
-                    "dir" : "seg_pancreas_ethrive_restrict",
+                    "dir" : "seg_pancreas_restrict",
                     "glob" : "pancreas_ff_lt_30.nii.gz",
                 },
                 "pancreas_ff_lt_50" : {
-                    "dir" : "seg_pancreas_ethrive_restrict",
+                    "dir" : "seg_pancreas_restrict",
                     "glob" : "pancreas_ff_lt_50.nii.gz",
                 },
                 "sat" : {
                     "dir" : "seg_sat_dixon_fix",
                     "glob" : "sat.nii.gz",
-                    "params" : []  # Volumes only
+                    "params" : ["ff"]  # Volumes only
                 },
                 "vat" : {
                     "dir" : "seg_vat_dixon",
@@ -159,20 +160,20 @@ class SegStats(statistics.SegStats):
                     "params" : []  # Volumes only
                 },
                 "kidney_cortex_l" : {
-                    "dir" : "seg_kidney_t1",
-                    "glob" : "kidney_cortex_l_t1.nii.gz"
+                    "dir" : "seg_kidney_t1_fix",
+                    "glob" : "kidney_cortex_l*.nii.gz"
                 },
                 "kidney_cortex_r" : {
-                    "dir" : "seg_kidney_t1",
-                    "glob" : "kidney_cortex_r_t1.nii.gz"
+                    "dir" : "seg_kidney_t1_fix",
+                    "glob" : "kidney_cortex_r*.nii.gz"
                 },
                 "kidney_medulla_l" : {
-                    "dir" : "seg_kidney_t1",
-                    "glob" : "kidney_medulla_l_t1.nii.gz"
+                    "dir" : "seg_kidney_t1_fix",
+                    "glob" : "kidney_medulla_l*.nii.gz"
                 },
                 "kidney_medulla_r" : {
-                    "dir" : "seg_kidney_t1",
-                    "glob" : "kidney_medulla_r_t1.nii.gz"
+                    "dir" : "seg_kidney_t1_fix",
+                    "glob" : "kidney_medulla_r*.nii.gz"
                 },
                 "kidney_dixon" : {
                     "dir" : "seg_kidney_dixon",
@@ -182,7 +183,7 @@ class SegStats(statistics.SegStats):
             },
             params={
                 "t2star" : {
-                    "dir" : "t2star",
+                    "dir" : "t2star_dixon",
                     "glob" : "t2star_exclude_fill.nii.gz",
                     "limits" : (2, 100),
                 },
@@ -208,7 +209,7 @@ class SegStats(statistics.SegStats):
                     "glob" : "b1.nii.gz",
                 },
                 "t2" : {
-                    "dir" : "t2",
+                    "dir" : "t2_scanner",
                     "glob" : "t2.nii.gz",
                 },
                 "se_t1" : {
@@ -218,6 +219,14 @@ class SegStats(statistics.SegStats):
                 "adc" : {
                     "dir" : "adc",
                     "glob" : "adc.nii.gz",
+                },
+                "t2_stim" : {
+                    "dir" : "t2",
+                    "glob" : "t2_stim.nii.gz",
+                },
+                "t2_exp" : {
+                    "dir" : "t2",
+                    "glob" : "t2_exp.nii.gz",
                 },
             },
             stats=["iqmean", "median", "iqstd", "mode", "fwhm"],
@@ -230,8 +239,9 @@ MODULES = [
     maps.FatFractionDixon(),
     maps.B0(),
     CopyModule("b1"),
-    T2(),
+    T2Scanner(),
     maps.T2starDixon(),
+    maps.T2(t2_dir="t2raw"),
     T1Liver(),
     T1Kidney(),
     SeT1Map(),
@@ -242,10 +252,10 @@ MODULES = [
     segmentations.SatDixon(),
     segmentations.BodyDixon(),
     segmentations.KidneyT1(),
-    segmentations.PancreasEthrive(),
+    segmentations.TotalSeg(src_dir="dixon", dilate=1),
     seg_postprocess.KidneyT1Clean(t2w=False),
     seg_postprocess.SegFix(
-        "seg_pancreas_ethrive",
+        "totalseg",
         fix_dir_option="pancreas_masks",
         segs={
             "pancreas.nii.gz" : "%s_*.nii.gz",
@@ -265,6 +275,16 @@ MODULES = [
         map_src=Module.INPUT,
     ),
     seg_postprocess.SegFix(
+        "seg_spleen_dixon",
+        fix_dir_option="spleen_masks",
+        segs={
+            "spleen.nii.gz" : "%s_*.nii.gz",
+        },
+        map_dir="dixon",
+        map_fname="water.nii.gz",
+        map_src=Module.INPUT,
+    ),
+    seg_postprocess.SegFix(
         "seg_sat_dixon",
         fix_dir_option="sat_masks",
         segs={
@@ -274,14 +294,45 @@ MODULES = [
         map_fname="water.nii.gz",
         map_src=Module.INPUT,
     ),
-    seg_postprocess.LargestBlob("seg_pancreas_ethrive_fix", "pancreas.nii.gz"),
+    seg_postprocess.SegFix(
+        "seg_kidney_t1",
+        fix_dir_option="kidney_masks",
+        segs={
+            "*cortex_l*.nii.gz": {
+                "glob": "%s_kidney.nii.gz",
+                "side": "left",
+                "fname": "kidney_cortex_l.nii.gz",
+            },
+            "*cortex_r*.nii.gz": {
+                "glob": "%s_kidney.nii.gz",
+                "side": "right",
+                "fname": "kidney_cortex_r.nii.gz",
+            },
+            # Currently we have no medulla fixes - these will fail
+            # to match so the original will be used if available
+            "*medulla_l*.nii.gz": {
+                "glob": "%s_kidney_medulla.nii.gz",
+                "side": "left",
+                "fname": "kidney_medulla_l.nii.gz",
+            },
+            "*medulla_r*.nii.gz": {
+                "glob": "%s_kidney_medulla.nii.gz",
+                "side": "right",
+                "fname": "kidney_medulla_r.nii.gz",
+            },
+        },
+        map_dir="t1_kidney",
+        map_fname="t1_map.nii.gz",
+        map_src=Module.OUTPUT,
+    ),
+    seg_postprocess.LargestBlob("totalseg_fix", "pancreas.nii.gz"),
     PancreasSegEro(),
     PancreasSegRestricted(),
     segmentations.VatDixon(
         organs={
             "seg_liver_dixon_fix" : "liver.nii.gz",
-            "seg_spleen_dixon" : "spleen.nii.gz",
-            "seg_pancreas_ethrive_fix_largestblob" : "pancreas.nii.gz",
+            "seg_spleen_dixon_fix" : "spleen.nii.gz",
+            "totalseg_fix_largestblob" : "pancreas.nii.gz",
             "seg_kidney_dixon" : "kidney.nii.gz"
         }
     ),
@@ -291,6 +342,7 @@ MODULES = [
 def add_options(parser):
     parser.add_argument("--pancreas-masks", help="Directory containing manual pancreas masks")
     parser.add_argument("--liver-masks", help="Directory containing manual liver masks")
+    parser.add_argument("--spleen-masks", help="Directory containing manual spleen masks")
     parser.add_argument("--sat-masks", help="Directory containing manual SAT masks")
     parser.add_argument("--kidney-masks", help="Directory containing manual kidney cortex/medulla masks")
     parser.add_argument("--se-t1-maps", help="Directory containing additional SE T1 maps")
